56edd7ee4938080b451e72ae6eb5f0dc18a058ce
SampathReddy12345 <sampathreddy139@gmail.com>
2021-06-25 15:33:35 +0530

    created a chat app for users to communicate

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '792099d7-8a42-44ce-b17f-e1e6a502c645';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '792099d7-8a42-44ce-b17f-e1e6a502c645';
 
 const Modal = () => {
   const [username, setUsername] = useState('');
